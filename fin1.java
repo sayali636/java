b099018e1ead4e43da4b83f4a57541540be62e16
sayali636 <sayalideokar987@gmail.com>
2025-07-25 09:27:27 +0100

    Add files via upload

---
+++ fin1.java
@@ -0,0 +1,10 @@
+class demo
+{
+   public static void main(String arg[])throws Exception
+    {
+      Runtime r=Runtime.getRuntime();
+           //r.exec("calc");  
+             r.exec("camera");  
+    }
+
+}
